2675dcca66dd4c126671c639a119190e119f3ce6
Subashini456 <subashiniravi492@gmail.com>
2021-03-25 01:23:58 +0530

    Added setter to orders and orderfoods, Made Owner menu update as link from button

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,11 +2,26 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_foods
 
+  before_save :set_total
+  before_save :set_total_order
+
   def total
     order_foods.collect { |order_food| order_food.valid? ? order_food.unit_price * order_food.quantity : 0 }.sum
   end
 
   def total_order
     order_foods.collect { |order_food| order_food.id }.count
+  end
+
+  private
+
+  def set_total
+    self[:total] = total
+  end
+
+  private
+
+  def set_total_order
+    self[:total_order] = total_order
   end
 end
--- app/models/order_food.rb
+++ app/models/order_food.rb
@@ -2,11 +2,22 @@ class OrderFood < ApplicationRecord
   belongs_to :menu
   belongs_to :order
 
+  before_save :set_unit_price
+  before_save :set_subtotal
+
   def unit_price
     menu.price
   end
 
   def subtotal
     unit_price * quantity
+  end
+
+  def set_unit_price
+    self[:unit_price] = unit_price
+  end
+
+  def set_subtotal
+    self[:subtotal] = subtotal
   end
 end
